248ecb05099ee1edae6db5a1fd52a8959f94e27f
pratikb0501 <pratikbagmare@gmail.com>
2025-06-18 21:43:01 -0700

    Changed url for user

--- userservice/src/main/java/com/p2plending/userservice/controller/UserController.java
+++ userservice/src/main/java/com/p2plending/userservice/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import com.p2plending.userservice.service.RegistrationService;
 
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
     private final RegistrationService registrationService;
